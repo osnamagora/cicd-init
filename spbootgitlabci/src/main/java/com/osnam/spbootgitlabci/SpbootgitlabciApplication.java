97ea82a1f0264c6fd992354e87901002f5a07bcb
osnamagora <osnam.agora@gmail.com>
2021-08-05 15:19:28 +0200

    initial commit

---
+++ spbootgitlabci/src/main/java/com/osnam/spbootgitlabci/SpbootgitlabciApplication.java
@@ -0,0 +1,13 @@
+package com.osnam.spbootgitlabci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootgitlabciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbootgitlabciApplication.class, args);
+    }
+
+}
---
+++ spbootgitlabci/src/test/java/com/osnam/spbootgitlabci/SpbootgitlabciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osnam.spbootgitlabci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootgitlabciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
